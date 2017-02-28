1d34f54f1a0b958b551a1e3e74d2af1299063140
ving <viing937@gmail.com>
2017-02-28 13:59:52 +0800

    optimize query performance

--- routes/block.js
+++ routes/block.js
@@ -5,14 +5,17 @@ var Express = require('express');
 var Router = Express.Router();
 
 var DbHelper = require('../js/dbHelper.js');
+var Ethereum = require('../js/ethereum.js');
 
 /* GET home page. */
 Router.get('/', function(req, res, next) {
-    var page = req.query.page ? parseInt(req.query.page) : 0;
+    var page = req.query.page ? parseInt(req.query.page) : 1;
     var pageSize = req.query.pageSize ? parseInt(req.query.pageSize) : 10;
-    DbHelper.queryBlcok({}, {
-        skip: pageSize * page,
-        limit: pageSize,
+
+    var start = parseInt(Ethereum.currentBlock) - page*pageSize;
+    var end = start + pageSize;
+
+    DbHelper.queryBlcok({number: {$gt: '0x'+start.toString(16), $lte: '0x'+end.toString(16)}}, {
         sort: {
             timestamp: -1
         }
